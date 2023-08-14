56dd1f257096496cb4fb47e1a5d8557b0f00739e
ask03 <aykim043@gmail.com>
2023-08-14 17:57:40 -0400

    init ERC721A

---
+++ src/HotelFuego.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.4;
+
+import "ERC721A/ERC721A.sol";
+
+contract HotelFuego is ERC721A {
+
+    constructor(string memory _name, string memory _symbol) ERC721A(_name, _symbol) {}
+
+}
